366c38797ebec00e303c946f2f7adeaee4c6ab3a
jieyou <3111825083@qq.com>
2022-09-05 15:40:59 +0800

    二

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 function tiaozhuan() {
   window.location.href = "jieyou.html";
 }
+function jia() {
+  let shuju = window.document.getElementById("shu");
+  let shuzi = parseInt(shuju.innerText);
+  shuju.innerText = shuzi + 1;
+}
